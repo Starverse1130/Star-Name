f9b07f67f8536810c40131635f47d8fb1989d7eb
Starverse <Starverse1130@gmail.com>
2026-03-26 14:31:10 +0530

    Basic designing

---
+++ script.js
@@ -0,0 +1,12 @@
+var Name = document.querySelector('h1')
+var btn = document.querySelector('#btn')
+
+// console.log(Name)
+
+// btn.addEventListener('mouseenter',function(){
+//     btn.innerHTML = 'Join us'
+// })
+
+// btn.addEventListener('mouseleave',function(){
+//     btn.innerHTML = 'Star'
+// })
